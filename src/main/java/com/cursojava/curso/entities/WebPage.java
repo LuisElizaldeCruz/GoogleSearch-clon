c05bd43d7568c2df8b23f975be67847737112d5c
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-02-28 00:18:42 -0600

    Creacion de la estructura del proyecto en SpringBoot

---
+++ src/main/java/com/cursojava/curso/entities/WebPage.java
@@ -0,0 +1,10 @@
+package com.cursojava.curso.entities;
+import jakarta.persistence.Entity;
+
+@Entity
+public class WebPage {
+    private String url;
+    private String title;
+    private String description;
+
+}
