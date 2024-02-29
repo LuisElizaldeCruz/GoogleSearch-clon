b1bc79aeb0c3e0280d94893fd9a4491b841b965b
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-02-29 00:05:25 -0600

    se agrego la conexion funcional con mysql

--- src/main/java/com/cursojava/curso/controllers/SearchController.java
+++ src/main/java/com/cursojava/curso/controllers/SearchController.java
@@ -1,10 +1,12 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.entities.WebPage;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -12,6 +14,10 @@ public class SearchController {
 
     @RequestMapping(value = "api/search", method = RequestMethod.GET)
     public List<WebPage> search(){
-        List<WebPagge> result = new ArrayList<>();
+        List<WebPage> result = new ArrayList<>();
+        WebPage page = new WebPage();
+        page.setTitle("test");
+        page.setDescription("test");
+        return result;
     }
 }
--- src/main/java/com/cursojava/curso/entities/WebPage.java
+++ src/main/java/com/cursojava/curso/entities/WebPage.java
@@ -1,10 +1,28 @@
 package com.cursojava.curso.entities;
-import jakarta.persistence.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Table(name = "WebPage")
+@Getter @Setter
+@ToString
 public class WebPage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "url")
     private String url;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "description")
     private String description;
 
 }
